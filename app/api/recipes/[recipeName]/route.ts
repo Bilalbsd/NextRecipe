0e810c1b13a670a52b5fe89ba2964813f7d83329
Bilal <dev.bilalb@gmail.com>
2024-06-22 22:41:25 +0200

    Add API route get recipe + recipe details page

--- app/api/recipes/[recipeName]/route.ts
+++ app/api/recipes/[recipeName]/route.ts
@@ -5,4 +5,24 @@ export async function GET(
   { params }: { params: { recipeName: string } }
 ) {
     console.log(params.recipeName, "params.recipeName");
+    const index = recipes.findIndex(
+        (food) => food.name.toLowerCase().replace(/ /g, '-') === params.recipeName
+    )
+    console.log(recipes[index]);
+    if(index !== -1) {
+        return new Response(JSON.stringify(recipes[index]), {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            status: 200
+        })
+    }
+    else {
+        return new Response("Food not found.", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            status: 404
+        });
+    } 
 }
